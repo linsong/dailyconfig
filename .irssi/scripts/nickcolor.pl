27f1eef3e044c00d5efc89c3a81a8a7d7a770b73
Vincent Wang <linsong.qizi@gmail.com>
2007-03-08 06:31:18 +0000

    add help for nickcolor.pl

--- .irssi/scripts/nickcolor.pl
+++ .irssi/scripts/nickcolor.pl
@@ -146,6 +146,15 @@ sub cmd_color {
       Irssi::print (chr (3) . "$i" . "Color #$i");
     }
   }
+  else {
+      irssi::print('%G>>%n nickcolor can give different color for different nick names');
+      irssi::print('%G>>%n available commands are: ');
+      irssi::print('%G>>%n   color list  -- list all nick names with color');
+      irssi::print('%G>>%n   color set nick_name color_number  -- set color for a nick name');
+      irssi::print('%G>>%n   color clear nick_name  -- clear color for a nick name ');
+      irssi::print('%G>>%n   color save  -- save modification ');
+      irssi::print('%G>>%n   color preview  -- preview all colors ');
+  }
 }
 
 load_colors;
