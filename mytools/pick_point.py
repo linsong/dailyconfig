a888f0459dbd7705b9c8780554ead458ab205548
Vincent Wang <linsong.qizi@gmail.com>
2007-03-19 11:34:48 +0000

    fix some problem of pick_point.py; add podget's configs into svn

--- mytools/pick_point.py
+++ mytools/pick_point.py
@@ -13,9 +13,11 @@ if __name__=="__main__":
         print " %s web_page_url" % sys.argv[0]
         sys.exit(1)
     next_page_url = sys.argv[1]
-    link_match_pat = re.compile(r'href=(http://[^>]+)>\xcf\xc2\xd2\xbb\xd2\xb3')
+    base_url = next_page_url[:next_page_url.rfind('/')]
+    link_match_pat = re.compile(r'href=([^>]+)>\xcf\xc2\xd2\xbb\xd2\xb3')
     txt_match_pat = re.compile(
-        r'^\s+\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a(.*)\xe3\x80\x80\xe5\x9b\x9e\xe5\xa4\x8d\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a')
+        r'^\s+\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a\s*([^\s]*)\s*(\xe6\x8f\x90\xe4\xba\xa4\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a|\xe3\x80\x80\xe5\x9b\x9e\xe5\xa4\x8d\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a)')
+    author_nick = sys.argv[2]
 
     while True:
         print "### fetching web page %s" % next_page_url
@@ -24,6 +26,8 @@ if __name__=="__main__":
         result = link_match_pat.search(source)
         if result and result.groups():
             next_page_url = result.groups()[0]
+            if not next_page_url.startswith('http://'):
+                next_page_url = base_url + '/' + next_page_url
         else:
             next_page_url = None
             break
@@ -37,7 +41,7 @@ if __name__=="__main__":
         while line:
             txt_mat_result = txt_match_pat.search(line)
             if txt_mat_result and txt_mat_result.groups():
-                if txt_mat_result.groups()[0]=='\xe6\x88\x91\xe7\x9a\x84\xe9\x82\xa3\xe7\x89\x87\xe7\xa2\xa7\xe6\xb5\xb7\xe8\x93\x9d\xe5\xa4\xa9':
+                if txt_mat_result.groups()[0]==author_nick:
                     start=True
                 else:
                     start=False
