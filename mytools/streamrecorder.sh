a380a545e3ef2ea85eefd695b24529f95cbf36ee
Vincent Wang <linsong.qizi@gmail.com>
2007-04-06 14:54:22 +0000

    update streamrecorder.sh: support to specify quality level from command line

--- mytools/streamrecorder.sh
+++ mytools/streamrecorder.sh
@@ -16,25 +16,32 @@ FILE_PREFIX=$(date +%Y-%m-%d-%M-%S)
 BUFFER_FILE=/tmp/stream_recorder_buffer_${FILE_PREFIX}
 #DEST_FILE=/home/vincent/download/mp3/studio_classroom/studio_classroom_${FILE_PREFIX}.mp3
 
-case :$1: in
-    :-t:)
-        shift
-        TIME_LENGTH=$1
-        shift
-        ;;
-    :-h:)
-        echo "USAGE: $cmdname [OPTIONS] download_file_name"
-        echo "OPTIONS"
-        echo ""
-        echo "  -t TIMELENGTH_IN_MINUTE  download how long stream"
-        echo "  -h                       print this help "
-        echo ""
-        exit 0 
-        ;;
-esac
+while [ $# -gt 1 ]; do
+    case :$1: in
+        :-t:)
+            shift
+            TIME_LENGTH=$1
+            ;;
+        :-q:)
+            shift
+            MP3_QUALITY=$1
+            ;;
+        :-h:)
+            echo "USAGE: $cmdname [OPTIONS] download_file_name"
+            echo "OPTIONS"
+            echo ""
+            echo "  -t TIMELENGTH_IN_MINUTE  download how long stream"
+            echo "  -h                       print this help "
+            echo ""
+            exit 0 
+            ;;
+    esac
+    shift
+done
 
 DEST_FILE=${1:?'You must specify a name for the download file.'}
 TIME_LENGTH=${TIME_LENGTH:-31} # default download 31 minutes
+MP3_QUALITY=${MP3_QUALITY:-2} # read man page of lame (-q) for more details 
 
 mkfifo $BUFFER_FILE
 # echo "Start to connecting to broadcaster ..."
@@ -43,6 +50,6 @@ mplayer -nolirc mms://211.89.225.101/live2 -ao pcm:file=$BUFFER_FILE -vc dummy -
 # stop recording after some minutes
 echo "kill $!" | at now + $TIME_LENGTH minutes
 
-lame -h $BUFFER_FILE  $DEST_FILE 
+lame -q $MP3_QUALITY $BUFFER_FILE  $DEST_FILE 
 rm $BUFFER_FILE
 
