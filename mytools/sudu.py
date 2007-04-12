73a40301d66c7f5146fa4a9209170229f1ed0491
Vincent Wang <linsong.qizi@gmail.com>
2007-04-12 02:05:36 +0000

    add more test data in sudu.py

--- mytools/sudu.py
+++ mytools/sudu.py
@@ -131,7 +131,41 @@ if __name__=="__main__":
     init_data2[7] = [0, 8, 0, 0, 7, 6, 0, 0, 0]
     init_data2[8] = [0, 6, 9, 0, 5, 0, 0, 1, 0]
 
-    start(init_data);
-    start(init_data1);
-    start(init_data2);
+    init_data3 = [ [] for i in range(9) ]
+    init_data3[0] = [0, 0, 0, 0, 0, 0, 6, 0, 5]
+    init_data3[1] = [0, 2, 0, 0, 0, 4, 7, 0, 9]
+    init_data3[2] = [0, 0, 7, 0, 0, 0, 0, 4, 3]
+    init_data3[3] = [0, 9, 0, 0, 2, 1, 3, 5, 0]
+    init_data3[4] = [0, 0, 0, 9, 0, 7, 0, 0, 0]
+    init_data3[5] = [0, 3, 4, 5, 6, 0, 0, 9, 0]
+    init_data3[6] = [9, 5, 0, 0, 0, 0, 8, 0, 0]
+    init_data3[7] = [1, 0, 6, 8, 0, 0, 0, 2, 0]
+    init_data3[8] = [8, 0, 3, 0, 0, 0, 0, 0, 0]
+
+    init_data4 = [ [] for i in range(9) ]
+    init_data4[0] = [9, 0, 4, 6, 0, 0, 0, 0, 5]
+    init_data4[1] = [0, 0, 0, 4, 0, 0, 0, 3, 0]
+    init_data4[2] = [0, 5, 1, 0, 0, 0, 0, 4, 0]
+    init_data4[3] = [8, 0, 2, 1, 0, 0, 0, 0, 0]
+    init_data4[4] = [0, 0, 0, 0, 5, 0, 8, 0, 9]
+    init_data4[5] = [6, 0, 0, 0, 9, 8, 4, 0, 2]
+    init_data4[6] = [0, 0, 0, 0, 3, 0, 0, 0, 0]
+    init_data4[7] = [0, 0, 0, 0, 6, 0, 2, 0, 3]
+    init_data4[8] = [5, 3, 6, 9, 0, 0, 7, 0, 0]
+
+    init_data5 = [ [] for i in range(9) ]
+    init_data5[0] = [5, 0, 0, 0, 0, 0, 2, 0, 1]
+    init_data5[1] = [0, 0, 0, 4, 5, 0, 0, 9, 3]
+    init_data5[2] = [0, 4, 0, 2, 3, 9, 0, 6, 0]
+    init_data5[3] = [0, 0, 0, 5, 0, 3, 0, 0, 6]
+    init_data5[4] = [0, 6, 0, 0, 0, 0, 0, 3, 0]
+    init_data5[5] = [9, 0, 0, 6, 0, 2, 0, 0, 0]
+    init_data5[6] = [0, 9, 0, 3, 4, 8, 0, 1, 0]
+    init_data5[7] = [3, 7, 0, 0, 6, 5, 0, 0, 0]
+    init_data5[8] = [6, 0, 4, 0, 0, 0, 0, 0, 9]
+
+
+    start(init_data3);
+    start(init_data4);
+    start(init_data5);
 
