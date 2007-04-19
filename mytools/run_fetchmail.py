40c825e8c6a154818e56b4047146f49a9bad9ad1
Vincent Wang <linsong.qizi@gmail.com>
2007-04-19 06:22:26 +0000

    add run_fetchmail.py that will ask password when loading fetchmail, in order to don't put plain text password in .fetchmailrc

---
+++ mytools/run_fetchmail.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+import os
+import re
+import getpass
+
+replacement_pat = re.compile(r'\$([^$]+)\$')
+
+if __name__=='__main__':
+    fetchmail_rc_path = os.path.join(os.environ['HOME'], '.fetchmailrc')
+    print "start to reading configs from %s" % fetchmail_rc_path
+    f = file(fetchmail_rc_path)
+    orig_config_lines = f.readlines()
+    f.close()
+
+    config_lines = []
+
+    print "start processing config ..."
+    for line in orig_config_lines:
+        line = line.lstrip()
+        if not line or line[0]=='#':
+            continue
+
+        m = replacement_pat.search(line)
+        if m and len(m.groups()):
+            passwd = getpass.getpass("Password for %s: " % m.groups()[0])
+            line = replacement_pat.sub(passwd, line)
+        config_lines.append(line)
+
+    print "launching fetchmail ..."
+    fetchmail_app = os.popen("fetchmail -f -", "w")
+    fetchmail_app.write("".join(config_lines))
+    fetchmail_app.close()
+
