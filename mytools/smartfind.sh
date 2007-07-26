01c6ec0b9ba0797f246b6c96f22512f731c582dd
Vincent Wang <linsong.qizi@gmail.com>
2007-07-26 06:11:24 +0000

    upload smartfind.sh

---
+++ mytools/smartfind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# smartfind.sh -- created 2007-07-26, Vincent Wang
+# @Last Change: 2007-07-26
+# @Revision:    0.1
+### How to use:
+###   $ ln -s smartfind.sh pyfind
+###   $ pyfind -inH blahblah   # search string 'blahbalh' from all python files
+
+BASE_NAME=$(basename "$0")
+EXT_NAME=${BASE_NAME/find/}
+
+if [ -n "${EXT_NAME}" ]; then 
+    #find . \( -type d -iname "lib" -prune \) -o \( -type d -iname "zope" -prune \) -o -type f -iname "*.py"  | xargs grep $*
+    find . -type f -iname "*.$EXT_NAME"  | xargs grep $*
+fi
+
+# vi: 
