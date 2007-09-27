79cab225441fd0c386a0cdb9ccb889adca021df6
Vincent Wang <linsong.qizi@gmail.com>
2007-09-27 02:36:24 +0000

    add some mozilla firefox's configuration to svn

---
+++ mozilla/firefox/user_pref.js
@@ -0,0 +1,45 @@
+// visit http://www.mozilla.org/unix/customizing.html#prefs for more details
+
+// keyconfig: key mappings 
+user_pref("keyconfig.main.addBookmarkAsKb", "!][][");
+user_pref("keyconfig.main.addYBookmarkAsKb", "][B][");
+user_pref("keyconfig.main.addYBookmarkAsKb_alt", "!][][");
+user_pref("keyconfig.main.f4kc_Back", "][,][");
+user_pref("keyconfig.main.f4kc_Back2", "!][][");
+user_pref("keyconfig.main.f4kc_BookmarkThisPage", "!][][");
+user_pref("keyconfig.main.f4kc_Find", "!][][");
+user_pref("keyconfig.main.f4kc_FindNext", "][N][");
+user_pref("keyconfig.main.f4kc_FindPrevious", "shift][N][");
+user_pref("keyconfig.main.f4kc_FocusNextFrame", "][F][");
+user_pref("keyconfig.main.f4kc_FocusURLBar", "][O][");
+user_pref("keyconfig.main.f4kc_Forward", "][.][");
+user_pref("keyconfig.main.f4kc_NextTab", "][]][");
+user_pref("keyconfig.main.f4kc_PrevTab", "][[][");
+user_pref("keyconfig.main.f4kc_Reload", "][R][");
+user_pref("keyconfig.main.f4kc_ReloadSkipCache", "shift][R][");
+user_pref("keyconfig.main.f4kc_ScrollBottom", "shift][G][");
+user_pref("keyconfig.main.f4kc_ScrollLineDown", "][J][");
+user_pref("keyconfig.main.f4kc_ScrollLineUp", "][K][");
+user_pref("keyconfig.main.f4kc_ScrollPageDown", "control][F][");
+user_pref("keyconfig.main.f4kc_ScrollPageUp", "control][B][");
+user_pref("keyconfig.main.f4kc_ScrollTop", "][G][");
+user_pref("keyconfig.main.f4kc_UpperDirectory", "control][U][");
+user_pref("keyconfig.main.f4kc_WebSearch", "control][K][");
+user_pref("keyconfig.main.goBackKb2", "!][][");
+user_pref("keyconfig.main.key_close", "][Q][");
+user_pref("keyconfig.main.key_find", "][/][");
+user_pref("keyconfig.main.key_findAgain", "control][G][");
+user_pref("keyconfig.main.key_newNavigatorTab", "][W][");
+user_pref("keyconfig.main.key_openYBookmarksSidebar", "shift][B][");
+user_pref("keyconfig.main.key_redo", "!][][");
+user_pref("keyconfig.main.key_stop", "!][][");
+user_pref("keyconfig.main.key_switchTextDirection", "!][][");
+user_pref("keyconfig.main.key_viewSource", "!][][");
+user_pref("keyconfig.main.viewBookmarksSidebarKb", "!][][");
+user_pref("keyconfig.main.webdeveloper-toolbar-key", "!][][");
+user_pref("keyconfig.main.xxx_key19_cmd_handleBackspace", "!][][");
+user_pref("keyconfig.version", 1);
+
+// middle mouse setting
+user_pref("middlemouse.contentLoadURL", true);
+
