47432dc7524216a8ed342b7d1cf7e3df25990e4c
Vincent Wang <linsong.qizi@gmail.com>
2009-08-14 10:05:12 +0000

    add named_scope to ruby tags file

---
+++ mytools/ftags.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+FILE_TAGS_NAME='file_tags'
+
+echo '!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;" to lines/' > $FILE_TAGS_NAME
+echo '!_TAG_FILE_SORTED	1	/0=unsorted, 1=sorted, 2=foldcase/' >> $FILE_TAGS_NAME
+echo '!_TAG_PROGRAM_AUTHOR	Darren Hiebert  /dhiebert@users.sourceforge.net/' >> $FILE_TAGS_NAME
+echo '!_TAG_PROGRAM_NAME	Exuberant Ctags //' >> $FILE_TAGS_NAME
+echo '!_TAG_PROGRAM_URL	http://ctags.sourceforge.net	/official site/' >> $FILE_TAGS_NAME
+echo '!_TAG_PROGRAM_VERSION	5.7	//' >> $FILE_TAGS_NAME
+
+find $* \
+    \( \
+        -name '*.jpg' -or \
+        -name '*.png' -or \
+        -name '*.swf' -or \
+        -name '*.gif' -or \
+        -name '*.ico' -or \
+        -name '*.swp' -or \
+        -name '*.swo' \
+    \) -prune -or \
+    \( \
+         -path '*/.svn/*' -or \
+         -path '*/migrate/*' -or \
+         -path '*/coverage/*' \
+     \) -prune -or \
+     -type f -print | \
+ruby -nle 'puts %Q{#{File.basename($_)}\t#{$_}\t#{%q{/^/;"}}\tr}' | \
+sort >> $FILE_TAGS_NAME
+
+exit 0
+
+
+###################################################################
+### below codes have some issues to debug
+###################################################################
+
+IGNORE_EXT_LIST=".jpg .png .swf .gif .ico .swp .swo"
+IGNORE_PATH_LIST=".svn migrate"
+
+FIND_CMD="find $* "
+
+FIRST_TIME='TRUE'
+FIND_CMD="$FIND_CMD "'\( '
+for ext in $IGNORE_EXT_LIST ; do 
+  if [ $FIRST_TIME == 'TRUE' ]; then 
+    FIRST_TIME='FALSE'
+  else
+    FIND_CMD="$FIND_CMD -or"
+  fi
+  FIND_CMD="$FIND_CMD -name \"*$ext\" "
+done
+FIND_CMD="$FIND_CMD "'\) -prune '
+
+FIRST_TIME='TRUE'
+FIND_CMD=" $FIND_CMD -or "'\( '
+for path in $IGNORE_PATH_LIST ; do 
+  if [ $FIRST_TIME == 'TRUE' ]; then 
+    FIRST_TIME='FALSE'
+  else
+    FIND_CMD="$FIND_CMD -or"
+  fi
+  FIND_CMD="$FIND_CMD -path \"*/$path/\" "
+done
+FIND_CMD="$FIND_CMD "'\) -prune '
+
+FIND_CMD="$FIND_CMD -or -type f -print "
+
+echo $FIND_CMD
+
+$FIND_CMD | ruby -nle 'result = []; result << File.basename($_); result << $_; result << %q{/^/;"} << "c"; puts result.join("\t")' | sort 
+
--- mytools/up.sh
+++ mytools/up.sh
@@ -1,9 +1,13 @@
+#! /usr/bin/env bash
+
 echo "updating codes ..."
 svn up 
 
 echo 
 echo "generate tags ..."
-ctags -R app lib test 
+# following command add named_scope to tags file
+myrtags -R app lib test
+#ctags -R --languages=Ruby --regex-Ruby='/^[ \t]*named_scope[ \t]+:([a-zA-Z0-9_]+),/\1/' app lib test
 
 echo
 echo "fixing ruby namespace issue in tags file ..."
@@ -12,7 +16,7 @@ echo "fixing ruby namespace issue in tags file ..."
 DIR_LIST="app test lib db config public"
 echo
 echo "generating file_tags for $DIR_LIST ..."
-~/tools/gen_rails_file_tags.sh $DIR_LIST
+~/mytools/ftags.sh $DIR_LIST
 
 echo
 echo "Done"
