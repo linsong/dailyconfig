fbc7db2886917ae601ded68f0cfe53882d1c045f
Vincent Wang <linsong.qizi@gmail.com>
2010-02-03 11:56:16 +0800

    mpd config id3 encoding change for mac os x;
    add a script to copy file name to id3 v1 tag

---
+++ mytools/fill_mp3_id3tag.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+echo "Delete all id tags ..."
+for i in *.mp3 ; do id3v2 -D "$i" ; done
+
+echo "Copy file name to id3 v1 tags"
+for i in *.mp3 ; do id3v2 -1 -t "$(basename "$i" .mp3)" "$i"; done
+
+echo "Done!"
